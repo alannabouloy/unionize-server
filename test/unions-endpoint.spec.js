9a7dcbb1dae142d4aab580bb756bfbbc438ae098
Alanna Bouloy <lanihope@gmail.com>
2021-01-29 20:51:34 -0500

    happy path tests all passing

--- test/unions-endpoint.spec.js
+++ test/unions-endpoint.spec.js
@@ -2,7 +2,7 @@ const supertest = require('supertest')
 const app = require('../src/app')
 const helpers = require('./test-helpers')
 
-describe.only('Unions endpoints', () => {
+describe('Unions endpoints', () => {
     let db
 
     const testIndustries = helpers.makeIndustriesArray()
@@ -73,6 +73,67 @@ describe.only('Unions endpoints', () => {
 
                 return supertest(app)
                     .get('/api/unions/?page=1&q=app')
+                    .expect(200)
+                    .expect(expectedResults)
+            })
+        })
+    })
+
+    describe('GET /api/unions/industry', () => {
+
+        beforeEach('insert industries', () => {
+            return helpers.seedIndustries(db, testIndustries)
+        })
+
+        beforeEach('insert unions', () => {
+            return helpers.seedUnions(db, testUnions)
+        })
+
+        context('without search', () => {
+            it('returns array of unions filtered by industry', () => {
+                let unions = [testUnions[0], testUnions[1]]
+                unions = unions.map(u => {
+                    return {
+                        name: u.name,
+                        desc: u.desc,
+                        industry: u.industry,
+                        webURL: u.webURL
+                    }
+                })
+
+                expectedResults = {
+                    unions,
+                    pageCount: 1,
+                    count: 2
+                }
+
+                return supertest(app)
+                    .get('/api/unions/industry/?page=1&industry=Industry%201')
+                    .expect(200)
+                    .expect(expectedResults)
+            })
+        })
+
+        context('with search', () => {
+            it('returns array of unions filtered by industry and search term', () => {
+                let unions = [testUnions[0]]
+                unions = unions.map(u => {
+                    return {
+                        name: u.name,
+                        desc: u.desc,
+                        industry: u.industry,
+                        webURL: u.webURL
+                    }
+                })
+
+                const expectedResults = {
+                    unions,
+                    pageCount: 1,
+                    count: 1
+                }
+
+                return supertest(app)
+                    .get('/api/unions/industry/?page=1&industry=Industry%201&q=app')
                     .expect(200)
                     .expect(expectedResults)
             })
